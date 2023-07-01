42854cec7980a838e072198f3131acd202c959dd
Mudit <muditert34@gmail.com>
2023-07-01 12:05:48 +0530

    clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>App</main>;
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
